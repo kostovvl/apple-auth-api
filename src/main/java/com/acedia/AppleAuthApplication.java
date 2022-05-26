a0ee8ed407a73d7265a07bb9bd98976b308f7ab9
Vladimir Kostov <vkostov@gannett.com>
2022-05-26 22:14:04 +0300

    Initial commit

---
+++ src/main/java/com/acedia/AppleAuthApplication.java
@@ -0,0 +1,13 @@
+package com.acedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppleAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acedia/AppleauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
