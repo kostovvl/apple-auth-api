156e33666a0229e40f3a39afa2f30783a1e480a7
Vladimir Kostov <vkostov@gannett.com>
2022-05-30 22:12:46 +0300

    Change to POST

--- src/main/java/com/acedia/uthorization/controller/AuthController.java
+++ src/main/java/com/acedia/uthorization/controller/AuthController.java
@@ -1,15 +1,12 @@
 package com.acedia.uthorization.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/validate-token")
 public class AuthController {
 
-    @GetMapping
+    @PostMapping
     public String test(@RequestHeader String code) {
         return code;
     }
