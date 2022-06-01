ddd1e222454a9db3239148899bc679b684cea4ce
Vladimir Kostov <vkostov@gannett.com>
2022-06-01 23:03:56 +0300

    Switch endpoint method

--- src/main/java/com/acedia/uthorization/controller/AuthController.java
+++ src/main/java/com/acedia/uthorization/controller/AuthController.java
@@ -2,14 +2,14 @@ package com.acedia.uthorization.controller;
 
 import com.acedia.uthorization.auth.user.AppleAuthorizationToken;
 import com.acedia.uthorization.service.AuthService;
+import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
-@RestController
+@Controller
 public class AuthController {
 
     private final AuthService authService;
@@ -18,8 +18,8 @@ public class AuthController {
         this.authService = authService;
     }
 
-    @GetMapping("/validate-token")
-    public AppleAuthorizationToken test(@RequestBody MultiValueMap<String, String> values) throws IOException {
+    @PostMapping("/validate-token")
+    public AppleAuthorizationToken validateToken(@RequestBody MultiValueMap<String, String> values) throws IOException {
         return authService.test(String.valueOf(values.get("code")));
     }
 }
